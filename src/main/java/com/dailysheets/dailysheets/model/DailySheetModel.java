eaaa13d5a564550f273938d3d419e70eefbc3758
Abby Henes <abbyhenes@gmail.com>
2022-01-27 10:44:03 -0600

    started on dailysheets model

--- src/main/java/com/dailysheets/dailysheets/model/DailySheetModel.java
+++ src/main/java/com/dailysheets/dailysheets/model/DailySheetModel.java
@@ -1,4 +1,59 @@
 package com.dailysheets.dailysheets.model;
 
 public class DailySheetModel {
+
+    private Long id;
+
+    private String date;
+
+    private String feeding1Time;
+    private Double feeding1Amt;
+    private String feeding2Time;
+    private Double feeding2Amt;
+    private String feeding3Time;
+    private Double feeding3Amt;
+    private String feeding4Time;
+    private Double feeding4Amt;
+    private String feeding5Time;
+    private Double feeding5Amt;
+
+    private String breakfastAmt;
+    private String lunchAmt;
+    private String snackAmt;
+
+    private String diaper1Time;
+    private String diaper1Description;
+    private String diaper2Time;
+    private String diaper2Description;
+    private String diaper3Time;
+    private String diaper3Description;
+    private String diaper4Time;
+    private String diaper4Description;
+    private String diaper5Time;
+    private String diaper5Description;
+    private String diaper6Time;
+    private String diaper6Description;
+
+    private String potty1Time;
+    private String potty1Description;
+    private String potty2Time;
+    private String potty2Description;
+    private String potty3Time;
+    private String potty3Description;
+    private String potty4Time;
+    private String potty4Description;
+    private String potty5Time;
+    private String potty5Description;
+    private String potty6Time;
+    private String potty6Description;
+
+    private String nap1Start;
+    private String nap1End;
+    private String nap2Start;
+    private String nap2End;
+
+    private String activity;
+
+    private String needs;
+
 }
