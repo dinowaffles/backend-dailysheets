1e7ea959299e985693542a4fbb7a6a00cb59dc57
Abby Henes <abbyhenes@gmail.com>
2022-01-27 14:41:54 -0600

    fleshed out classroom controller

--- src/main/java/com/dailysheets/dailysheets/controller/ClassroomController.java
+++ src/main/java/com/dailysheets/dailysheets/controller/ClassroomController.java
@@ -1,11 +1,13 @@
 package com.dailysheets.dailysheets.controller;
 
+import com.dailysheets.dailysheets.model.ClassroomModel;
 import com.dailysheets.dailysheets.service.ClassroomService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -22,13 +24,32 @@ public class ClassroomController {
         this.classroomService = classroomService;
     }
 
-
     // GET all classrooms
+    @GetMapping("/classroom")
+    public List<ClassroomModel> getClassrooms() {
+        LOGGER.info("controller calling getClassrooms");
+        return classroomService.getClassrooms();
+    }
 
     // POST create a classroom
+    @PostMapping("/classroom")
+    public ClassroomModel addClassroom(@RequestBody ClassroomModel classroomObject) {
+        LOGGER.info("controller calling addClassroom");
+        return classroomService.addClassroom(classroomObject);
+    }
 
     // PUT update a classroom
+    @PutMapping("/classroom/{classroomId}")
+    public ResponseEntity<ClassroomModel> updateClassroom(
+            @PathVariable Long classroomId, @RequestBody ClassroomModel classroomObject) {
+        LOGGER.info("controller calling updateClassroom");
+        return classroomService.updateClassroom(classroomId, classroomObject);
+    }
 
     // DEL delete a classroom
-
+    @DeleteMapping("/classroom/{classroomId}")
+    public ResponseEntity<Map<String, Boolean>>  deleteClassroom(@PathVariable Long classroomId) {
+        LOGGER.info("controller calling deleteClassroom");
+        return classroomService.deleteClassroom(classroomId);
+    }
 }
