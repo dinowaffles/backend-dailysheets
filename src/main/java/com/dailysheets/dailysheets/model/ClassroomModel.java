eadd22ea4841e406e6d3820eb8c51ef10ab89bce
Abby Henes <abbyhenes@gmail.com>
2022-01-27 09:46:57 -0600

    scaffolded classroom model

--- src/main/java/com/dailysheets/dailysheets/model/ClassroomModel.java
+++ src/main/java/com/dailysheets/dailysheets/model/ClassroomModel.java
@@ -1,4 +1,49 @@
 package com.dailysheets.dailysheets.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "classrooms")
 public class ClassroomModel {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String ageGroup;
+
+    public ClassroomModel(Long id, String name, String ageGroup) {
+        this.id = id;
+        this.name= name;
+        this.ageGroup = ageGroup;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAgeGroup() {
+        return ageGroup;
+    }
+
+    public void setAgeGroup(String ageGroup) {
+        this.ageGroup = ageGroup;
+    }
 }
